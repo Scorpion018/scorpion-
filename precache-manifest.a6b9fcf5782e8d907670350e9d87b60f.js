918797659058db0ab90acf310f930d3f77fee6f5
Scorpion018 <vishugowda2017@gmail.com>
2021-11-07 22:19:44 +0530

    Updates

---
+++ precache-manifest.a6b9fcf5782e8d907670350e9d87b60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7008f8c9125a58887ed5bbac779d8650",
+    "url": "/tutorials/index.html"
+  },
+  {
+    "revision": "9ed23292116fd7e597a1",
+    "url": "/tutorials/static/css/main.8651309b.chunk.css"
+  },
+  {
+    "revision": "ead6e6fd9cfda86b82bf",
+    "url": "/tutorials/static/js/2.38851006.chunk.js"
+  },
+  {
+    "revision": "38e2c94dc3be9404d59269b34c2440f6",
+    "url": "/tutorials/static/js/2.38851006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed23292116fd7e597a1",
+    "url": "/tutorials/static/js/main.1ef73abc.chunk.js"
+  },
+  {
+    "revision": "b2852d204570de4e4f65",
+    "url": "/tutorials/static/js/runtime-main.99e31c81.js"
+  }
+]);
